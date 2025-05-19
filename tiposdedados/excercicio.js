dba26e7b3596916e89e8adad3e6f1b1384c5228a
Walisson Santos <walissonsantost008@gmail.com>
2025-05-19 19:30:09 -0300

    Tipos de dados
    Vendo os tipos de dados do js

---
+++ tiposdedados/excercicio.js
@@ -0,0 +1,20 @@
+// Declare uma variável contendo uma string
+
+// Declare uma variável contendo um número dentro de uma string
+
+// Declare uma variável com a sua idade
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+// Coloque a seguinte frase em uma variável: It's time
+
+// Verifique o tipo da variável que contém o seu nome
+
+let nome2 = 'walisson'
+let numero = '18'
+let idade2 = 17
+let sobrenome2= 'Santos'
+let nomeCompleto2 = nome + ' ' + sobrenome
+console.log(nomeCompleto2)
+console.log(typeof nome)
---
+++ tiposdedados/script.js
@@ -0,0 +1,23 @@
+let nome = 'walisson' // String
+console.log(typeof nome)
+
+let idade = 17 // Number
+console.log(typeof idade)
+
+let possuiFaculdade = false // Bolean
+console.log(typeof possuiFaculdade)
+
+let time; // Underfuned
+console.log(typeof time)
+
+let simbolo = Symbol() // Symbol
+console.log(typeof simbolo)
+
+let novoObjeto = {} // Object
+console.log(typeof novoObjeto)
+
+let sobrenome = 'Santos'
+let nomeCompleto = nome + ' ' + sobrenome
+console.log(nomeCompleto)
+
+
