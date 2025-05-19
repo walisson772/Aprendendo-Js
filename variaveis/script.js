e3de510e1bcbe82a59df7777970d367c421d1fcb
Walisson Santos <walissonsantost008@gmail.com>
2025-05-19 19:15:50 -0300

    variaveis
    Aprendendo a como declarar variaveis e os tipo: var, let e const

---
+++ variaveis/script.js
@@ -0,0 +1,12 @@
+
+// const -> serve para criar constantes(Não muda o valor)
+const name = 'walisson'
+const dataNascimento = '2008/01/30'
+
+// let -> Pode mudar o valor, é melhor usar o let no lugar do var.
+let idade = 17
+
+// var -> É mais recomendado usar o let
+var digaOi = 'oi'
+
+console.log(`Olá ${name} prazer em te conhecer` )
